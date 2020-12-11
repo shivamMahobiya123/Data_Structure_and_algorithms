63bcc9b11d63162c584bd7c1570d669340f376c3
Shivam Mahobiya <48170409+shivamMahobiya123@users.noreply.github.com>
2020-12-11 15:53:35 +0530

    Update linearsearch.cpp

--- linearsearch.cpp
+++ linearsearch.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 int main()
 {
-
+ int n ,array[100],pos,k; 
+  for(int i=0;i<n-1;i++)
+  {
+    cin>>array[i];
+  }
+  cin>>k;
+  for(int i=0;i<n-1;i++)
+  {
+    if(array[i]==k)
+    { 
+      pos=i;
+      break;
+    }
+  }
+  cout<<k<<"is in position :"<<pos+1;
 return 0;
 }
