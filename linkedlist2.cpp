30cfd096c375671e2762c1ba8c99f502a48a471c
Shivam Mahobiya <48170409+shivamMahobiya123@users.noreply.github.com>
2021-01-08 13:02:43 +0530

    Update linkedlist2.cpp

--- linkedlist2.cpp
+++ linkedlist2.cpp
@@ -1,1 +1,37 @@
-
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+ //singly linked list
+  //In this linked list,it stores address of next node only.
+  struct simplynode
+  {
+    int a ;
+    struct simplynode *next1;
+  };
+  //doubly linked list
+  //In this linked list,it stores address of previous node and next node .
+    struct doublynode
+  {
+    int b ;
+    struct doublynode *next2;
+    struct doublynode *prev2; 
+  };
+  //circular  linked list
+  //In this linked list,it stores address of next node .At last node contains address of first node only.
+    struct circularnode
+  {
+    int c ;
+    struct circularnode *next1;
+  };
+  
+  //doubly circuar linked list
+  // In this linked list,it stores address of previous node and next node .At last node contains address of first node and also first node to last node
+    struct doublycircularnode
+  {
+    int d ;
+     struct doublycircularnode *next2;
+    struct doublycircularnode *prev2;   
+   };
+ return 0;
+}
