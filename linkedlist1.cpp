379fbd27356ac56c9586e59bd81a1931c24d6004
Shivam Mahobiya <48170409+shivamMahobiya123@users.noreply.github.com>
2021-01-08 11:28:42 +0530

    Introduction of linkedlist
    How to declaration of linked list

---
+++ linkedlist1.cpp
@@ -0,0 +1,10 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{ 
+  struct node //declaration of linked list
+  {
+  int a; //insertion of data 
+  struct node *next; //pointer to store address of the next address
+  }
+}
